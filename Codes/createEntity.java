2b09c5ac2c66b9de466d75db34e6b18b5a10eefd
abhit-rana <abhit20421@iiitd.ac.in>
2021-12-21 21:41:43 +0530

    made changes to the images

---
+++ Codes/createEntity.java
@@ -0,0 +1,10 @@
+package com.example.willherojavafxproject;
+
+public class createEntity extends Thread{
+
+    private String[] entities = new String[]{};
+    @Override
+    public void run(){
+
+    }
+}
